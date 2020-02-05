95c1d0d09b38f38c96820d13ee230b1e605536a0
Jon Duckworth <bigduck@gmail.com>
2020-02-04 22:06:37 -0700

    Add option to specify a Docker image to use for the build

--- lambchops/build.py
+++ lambchops/build.py
@@ -33,7 +33,8 @@ def get_resource_path(package, resource):
 @click.option('--as-layer', '-l', is_flag=True, help='Whether to compile the zip file as a Lambda layer. If the flag is present, the zip archive will support '
                                                'uploading as a Lambda Layer. If it is not present, the zip archive will support uploading as a Lambda Function.')
 @click.option('--output-name', '-o', default='package', help='The name of the output zip file (and the associated folder). The zip file will be named {output_name}.zip.')
-def build(build_context, output_dir, runtime, as_layer, output_name):
+@click.option('--image', '-i', help='The Docker image to use when building the layer/function. Defaults to one of the lambci/lambda:build-* images.')
+def build(build_context, output_dir, runtime, as_layer, output_name, image):
     """
     Compiles a zip archive that can be uploaded to AWS Lambda as either a Function or a Layer. The BUILD_CONTEXT must contain a requirements.txt file that lists
     all of the Python dependencies. If you are compiling a Layer, that's all you need. If you are compiling a Function, you should put all of your Python code
@@ -45,8 +46,10 @@ def build(build_context, output_dir, runtime, as_layer, output_name):
     compilers_dir = get_resource_path('lambchops', 'compilers/')
     compiler_name = COMPILER_MAPPING[runtime]
 
+    image = image or f'lambci/lambda:build-{runtime}'
+
     client.containers.run(
-        f'lambci/lambda:build-{runtime}',
+        image,
         f'sh -e "/src/{compiler_name}" {"layer" if as_layer else "function"} "{output_name}"',
         volumes={
             build_context: {'bind': '/mnt/app', 'mode': 'ro'},
