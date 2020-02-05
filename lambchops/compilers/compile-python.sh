8068fce3258ae0164b29088ddc2b6495b5c61e99
Jon Duckworth <bigduck@gmail.com>
2020-02-04 21:54:57 -0700

    Clear uncompiled Python files when building

--- lambchops/compilers/compile-python.sh
+++ lambchops/compilers/compile-python.sh
@@ -16,13 +16,28 @@ rm -rf ${zip_name};
 
 mkdir -p ${compile_dir};
 
-if [[ -f /mnt/app/requirements.txt ]];
+if [ -f /mnt/app/requirements.txt ];
 then
 	pip install -r /mnt/app/requirements.txt -t ${compile_dir} --upgrade;
 fi
 
 cp -r /mnt/app/* ${compile_dir}/;
 rm -rf ${compile_dir}/requirements.txt;
+
+# Remove Lambda Python packages
+rm -rdf ${compile_dir}/boto3/ \
+  && rm -rdf ${compile_dir}/botocore/ \
+  && rm -rdf ${compile_dir}/docutils/ \
+  && rm -rdf ${compile_dir}/dateutil/ \
+  && rm -rdf ${compile_dir}/jmespath/ \
+  && rm -rdf ${compile_dir}/s3transfer/ \
+  && rm -rdf ${compile_dir}/numpy/doc/
+
+# Remove uncompiled Python scripts
+find ${compile_dir} -type f -name '*.pyc' | while read f; do n=$(echo $f | sed 's/__pycache__\///' | sed 's/.cpython-36//'); cp $f $n; done;
+find ${compile_dir} -type d -a -name '__pycache__' -print0 | xargs -0 rm -rf
+find ${compile_dir} -type f -a -name '*.py' -print0 | xargs -0 rm -f
+
 
 cd /tmp/outputs/${OUTPUT_NAME};
 
